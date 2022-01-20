c35272c4b3ca265f4873cba09755b34f8ce9d72c
victoriapcela <74787520+victoriapcela@users.noreply.github.com>
2022-01-20 23:14:44 +0200

    Update app.js

---
+++ Task1/app.js
@@ -0,0 +1,25 @@
+let n = prompt("Введите цифры от 0 до 9");
+switch(n) {
+    case '0' : alert("Ноль");
+    break;
+    case '1' : alert("Один");
+    break;
+    case '2' : alert("Два");
+    break;
+    case '3' : alert("Три");
+    break;
+    case '4' : alert("Четыре");
+    break;
+    case '5' : alert("Пять");
+    break;
+    case '6' : alert("Шесть");
+    break;
+    case '7' : alert("Семь");
+    break;
+    case '8' : alert("Восемь");
+    break;
+    case '9' : alert("Девять");
+    break;
+    default: alert("Ошибка");
+    break;
+}
