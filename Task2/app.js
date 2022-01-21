ae4999d427c0d5f8f712ff8c358a6ffaf9575feb
victoriapcela <74787520+victoriapcela@users.noreply.github.com>
2022-01-21 17:59:16 +0200

    2

---
+++ Task2/app.js
@@ -0,0 +1,7 @@
+let str = prompt("Введите свое имя");
+alert(`Hi ${str}`);
+if (confirm("Вы знаете JS?")) {
+  alert("Молодец!")
+} else {
+  alert("You can learn it right now")
+}
---
+++ Task3/app.js
@@ -0,0 +1,8 @@
+let n = prompt("Introdu numarul");
+if ( n > 0) {
+  alert("1")
+} else if ( n == 0){
+  alert("0")
+} else if ( n < 0) {
+  alert("-1")
+} else { alert("Error")}
